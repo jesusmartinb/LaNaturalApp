b32d4720ba62f9698490ac417786dc469f8c2c48
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-05-02 00:00:23 +0200

    Se eliminan los estilos por defecto de los archivos index.css y App.css. En el componente Layout se crea un navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Layout = ({ children }) => {
     return (
-        <div>
-            <h1>Layout</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/search" >Buscar</Link></li>
-                    <li><Link to="/carrito" >Carrito</Link></li>
-                </ul>
-            </nav>
+        <>
+            <Navbar expand="lg" className="bg-success-subtle">
+                <Container>
+                    <Navbar.Brand href="/">La Natural</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/search">Buscar</Nav.Link>
+                        <Nav.Link href="/carrito">Carrito</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <main>{children}</main>
-        </div>
+        </>
     )
 }
 
